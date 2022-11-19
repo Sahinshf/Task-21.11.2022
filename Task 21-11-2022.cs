414d30ae787a76c716e683841c8372a1964f2198
Sahinshf <shahinrsha@code.edu.az>
2022-11-19 21:23:59 +0400

    Task is here

---
+++ Task 21-11-2022.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_21_11_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            calculator(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Console.ReadLine());
+        }
+        
+        static void calculator(double num1 , double num2 , string z)
+        {
+            if( z == "+" )
+            {
+                Console.WriteLine(num1+num2);
+            }
+            else if (z == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (z == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (z == "/")
+            {
+                double result = num1 / num2;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
